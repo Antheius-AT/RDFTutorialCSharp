516969b922c524c77552c5701c7fffcfa62b70b9
TTMVP-At <tom.pirich@outlook.com>
2021-01-19 20:20:29 +0100

    Added initial structure of a tupel.

---
+++ RDFSharp/RDFTutorialLogic/Triple.cs
@@ -0,0 +1,32 @@
+﻿namespace RDFTutorialLogic
+{
+    /// <summary>
+    /// Represents the class for storinn the data of a triple.
+    /// </summary>
+    internal class Triple
+    {
+        /// <summary>
+        /// Gets or sets the subject of the triple.
+        /// </summary>
+        public string Subject
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets the predicate of the triple.
+        /// </summary>
+        public string Predicate
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets the object of the triple.
+        /// </summary>
+        public string @Object
+        {
+            get;
+        }
+    }
+}
