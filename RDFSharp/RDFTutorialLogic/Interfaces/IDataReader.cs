945d53309d26474418a5e279666fa6dd6b1d9b21
TTMVP-At <tom.pirich@outlook.com>
2021-01-24 15:13:15 +0100

    Added interface for a reader.

--- RDFSharp/RDFTutorialLogic/Interfaces/IDataReader.cs
+++ RDFSharp/RDFTutorialLogic/Interfaces/IDataReader.cs
@@ -19,6 +19,6 @@ namespace RDFTutorialLogic.Interfaces
         /// </summary>
         /// <param name="path">The path to the file.</param>
         /// <returns>A collection of raw triple data which will be processed further.</returns>
-        IEnumerable<RawTripleData> Read(string path);
+        IEnumerable<CsvTripleDataFormat> Read(string path);
     }
 }
