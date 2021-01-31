b309f4c11df56c51b5de1c302056497e03dd6d12
TTMVP-At <tom.pirich@outlook.com>
2021-02-01 00:39:14 +0100

    Added some tests.

--- RDFSharp/RDFTutorialLogic/Program.cs
+++ RDFSharp/RDFTutorialLogic/Program.cs
@@ -25,37 +25,97 @@ namespace RDFTutorialLogic
             var uriPrefix = "RDFDemoLibrary";
             var reader = new CSVDataReader();
             var parser = new TripleParser(uriPrefix);
-            var data = reader.Read(@"F:\FH_Stuff\3_Semester\SemantischeTechnologien\Aufgabenstellung\RDFTutorialCSharp\TestCSVData\Test2.csv");
-            var tripleStore = new TripleStore(uriPrefix);
+            //var data = reader.Read(@"F:\FH_Stuff\3_Semester\SemantischeTechnologien\Aufgabenstellung\RDFTutorialCSharp\TestCSVData\Test2.csv");
+            //var tripleStore = new TripleStore(uriPrefix);
             var reasoner = new Reasoner();
-            reasoner.RegisterRule(new InverseDependencyRule("Ist beFReundet mit", uriPrefix));
+            //reasoner.RegisterRule(new InverseDependencyRule("Ist beFReundet mit", uriPrefix));
 
-            var triples = tripleStore.RetrieveMatchingTriplesAsync(null, null, null);
+            //var triples = tripleStore.RetrieveMatchingTriplesAsync(null, null, null);
 
-            foreach (var item in data)
+            //foreach (var item in data)
+            //{
+            //    try
+            //    {
+            //        var triple = parser.Parse(item);
+            //        tripleStore.TryAddTriple(triple);
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Console.WriteLine("Du bist ein Volltrottel.");
+            //    }
+            //}
+
+            //foreach (var item in triples)
+            //{
+            //    Console.WriteLine(item.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
+            //}
+
+            //Console.WriteLine("------------------------------------------------------------");
+            //var reasoned = reasoner.InvokeRules(triples);
+
+            //foreach (var item in reasoned)
+            //{
+            //    Console.WriteLine(item.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
+            //}
+            var predRule1 = new TransitiveDependencyRule(
+                $"{uriPrefix.ToLower()}:ist zusammen mit", 
+                $"{uriPrefix.ToLower()}:gehört",
+                basePredicateIsRelatedToSubject: false,
+                mappedPredicateIsRelatedToSubject: false,
+                uriPrefix);
+
+            var predRule2 = new TransitiveDependencyRule(
+                $"{uriPrefix.ToLower()}:hat Schulden bei",
+                $"{uriPrefix.ToLower()}:gehört",
+                basePredicateIsRelatedToSubject: true,
+                mappedPredicateIsRelatedToSubject: false,
+                uriPrefix);
+
+            var predRule3 = new TransitiveDependencyRule(
+                $"{uriPrefix.ToLower()}:ist",
+                $"{uriPrefix.ToLower()}:hat",
+                basePredicateIsRelatedToSubject: false,
+                mappedPredicateIsRelatedToSubject: true,
+                uriPrefix);
+
+
+            reasoner.RegisterRule(predRule1);
+            reasoner.RegisterRule(predRule2);
+            reasoner.RegisterRule(predRule3);
+            var rawData = reader.ReadFiles(
+                @"E:\VisualStudio\Visual C#\RDFTutorialCSharp\TestCSVData\Test2.csv");
+            var tripleData = new List<RDFTriple>();
+            foreach (var rawTripleData in rawData)
             {
                 try
                 {
-                    var triple = parser.Parse(item);
-                    tripleStore.TryAddTriple(triple);
+                    tripleData.Add(parser.Parse(rawTripleData));
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Du bist ein Volltrottel.");
                 }
+               
             }
 
-            foreach (var item in triples)
+
+            Console.WriteLine("Starting data:");
+            foreach (var triple in tripleData)
             {
-                Console.WriteLine(item.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
+                Console.WriteLine(triple.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
             }
+            Console.WriteLine("-----------------------");
 
-            Console.WriteLine("------------------------------------------------------------");
-            var reasoned = reasoner.InvokeRules(triples);
+            Console.WriteLine("Rules:");
 
-            foreach (var item in reasoned)
+            Console.WriteLine("Predicate rule 1: ist zusammen mit => gehört");
+            Console.WriteLine("Predicate rule 1: hat Schulden bei => gehört");
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Inferred data:");
+            tripleData = reasoner.InvokeRules(tripleData).ToList();
+            foreach (var triple in tripleData.Distinct())
             {
-                Console.WriteLine(item.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
+                Console.WriteLine(triple.ToString().Replace($"{uriPrefix.ToLower()}:", string.Empty));
             }
 
             Console.ReadKey(true);
