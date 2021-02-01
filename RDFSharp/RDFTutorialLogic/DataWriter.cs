d46b6f86fc617d37ce8d4b3f9e873a60377d4c34
TTMVP-At <tom.pirich@outlook.com>
2021-02-01 12:28:22 +0100

    Writing triple to an file.

---
+++ RDFSharp/RDFTutorialLogic/DataWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDFTutorialLogic
+{
+    using FileHelpers;
+    using Interfaces;
+    public class DataWriter : IDataWriter
+    {
+        private readonly FileHelperEngine<>
+        public DataWriter()
+        {
+            
+        }
+    }
+}
---
+++ RDFSharp/RDFTutorialLogic/Interfaces/IDataWriter.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IDataReader.cs" company="FHWN">
+//     Copyright (c) FHWN. All rights reserved.
+// </copyright>
+// <author>Tom Pirich</author>
+//-----------------------------------------------------------------------
+namespace RDFTutorialLogic.Interfaces
+{
+    using System.Collections.Generic;
+    using RDFTutorialLogic.Data;
+
+    /// <summary>
+    /// Represents the interface for writing data to an external source.
+    /// </summary>
+    public interface IDataWriter
+    {
+        /// <summary>
+        /// Writes 
+        /// </summary>
+        /// <param name="patha"></param>
+        void Write(Ontology ontolgy, string path);
+    }
+}
