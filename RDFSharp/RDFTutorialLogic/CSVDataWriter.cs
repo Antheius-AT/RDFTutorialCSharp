b53c13b7c9c09fd2486aee933afca90a3225bac2
TTMVP-At <tom.pirich@outlook.com>
2021-02-01 12:36:59 +0100

    Initial layout of the data writer.

---
+++ RDFSharp/RDFTutorialLogic/CSVDataWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDFTutorialLogic
+{
+    using FileHelpers;
+    using Interfaces;
+    using RDFTutorialLogic.Data;
+
+    public class CSVDataWriter : IDataWriter
+    {
+        private readonly FileHelperEngine<RawTripleData> writer;
+        public CSVDataWriter()
+        {
+            this.writer = new FileHelperEngine<RawTripleData>();
+        }
+
+        public void Write(Ontology ontolgy, string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
